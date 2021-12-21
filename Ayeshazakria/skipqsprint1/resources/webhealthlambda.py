38b6df60c7faa32e567c74e24f0a4257189b943e
EC2 Default User <ec2-user@ip-172-31-34-211.us-east-2.compute.internal>
2021-12-21 17:42:01 +0000

    added s3 bucket

--- Ayeshazakria/skipqsprint1/resources/webhealthlambda.py
+++ Ayeshazakria/skipqsprint1/resources/webhealthlambda.py
@@ -2,46 +2,48 @@ import datetime
 import urllib3
 import constant as constants
 from cloudwatch import Cloudwatchputmetrics
+from uploadtobucket import upload_file, get_file
 
 def lambda_handler(event,context):
+    urls = get_file("ayeshaskipqbucket","url_list.txt")
     values =dict()
     cw=Cloudwatchputmetrics()
-    avail=get_availability()
-    dimensions=[ 
+    for url in urls:
+        avail=get_availability(url)
+        dimensions=[ 
         { "Name":"URL",
-          "Value":constants.URL_TO_MONITOR
+          "Value":url
         },
         {  "Name":"Region", 
            "Value":" DUB"
         }
         ]
-    cw.put_data(constants.URL_MONITOR_NAMESPACE,constants.URL_MONITOR_NAME_AVAILABILITY,dimensions,avail)
-    latency=get_latency()
-    dimensions=[ 
+        cw.put_data(url,"url_availability",dimensions,avail)
+        latency=get_latency(url)
+        dimensions=[ 
         { "Name":"URL",
-          "Value":constants.URL_TO_MONITOR
+          "Value":url
         },
         {  "Name":"Region", 
            "Value":" DUB" 
         }
         ]
     
-    cw.put_data(constants.URL_MONITOR_NAMESPACE,constants.URL_MONITOR_NAME_LATENCY,dimensions,latency)
-    
-    values.update({"availability":avail, "Latency":latency})
+        cw.put_data(url,"url_latency",dimensions,latency)
+        values.update({"availability":avail, "Latency":latency})
     return values
     
-def get_availability():
+def get_availability(url):
     http=urllib3.PoolManager()
-    response = http.request("GET",constants.URL_TO_MONITOR)
+    response = http.request("GET",url)
     if response.status==200:
         return 1.0
     else:
         return 0.0
-def get_latency():
+def get_latency(url):
     http= urllib3.PoolManager()
     start=datetime.datetime.now()
-    response = http.request("GET",constants.URL_TO_MONITOR)
+    response = http.request("GET",url)
     end = datetime.datetime.now()
     delta=end-start
     latencySec=round(delta.microseconds * .000001,6)
