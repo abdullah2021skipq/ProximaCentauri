6dc48944fec1173fd00f798ac31738d31aa98772
EC2 Default User <ec2-user@ip-172-31-38-191.us-east-2.compute.internal>
2021-12-29 21:18:08 +0000

    S3

--- Adeel/Sprint3/AdeelProject3/resources1/db_s3_lambda.py
+++ Adeel/Sprint3/AdeelProject3/resources1/db_s3_lambda.py
@@ -12,12 +12,12 @@ def lambda_handler(event, context):
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
     
-    Url_Monitor= bo('adeelskipq','urls.json').bucket_as_list()
+    Url_Monitor= bo(bucket_name,key).bucket_as_list()
     
     table_name = os.getenv('table_name')#getting table name
     
     for link in Url_Monitor:
         client.put_item(
-        TableName = 'BetaStage-AdeelStack-Urls816F5996-1NK3XUOOLE39F',
+        TableName = table_name,
         Item={'Links':{'S': link}
         })
