08f2deb5c68a27c61cadfcd3ddaf5f17f791dc56
Ubuntu <ubuntu@ip-172-31-39-2.us-east-2.compute.internal>
2021-12-29 22:14:36 +0000

    sprint3 dynamo

--- AbdullahZaman/sprint3/PCRepoAZB/resources/sprint3_dynamo.py
+++ AbdullahZaman/sprint3/PCRepoAZB/resources/sprint3_dynamo.py
@@ -1,35 +1,41 @@
 import boto3
-from resources import s3bucket
+import s3bucket
+import time
 
 def create_sprint3_table():
-    client_ = boto3.resource('dynamodb', region_name='us-east-2')
+    client_ = boto3.resource('dynamodb')
     try:
-        table = client_.create_table(TableName='AbdullahSprint3',
-                                KeySchema=[
-                                {
-                                'AttributeName': 'URL_ADDRESS',
-                                'KeyType': 'HASH'
-                                }
-                    ],
-                                AttributeDefinitions=[
-                                {
-                                'AttributeName': 'URL_ADDRESS',
-                                'AttributeType' : 'S'
-                                }
-                                ]
-                    )
+        table = client_.create_table(
+            TableName='AbdullahSprint3',
+            KeySchema=[
+                {
+                    'AttributeName': 'URL_ADDRESS',
+                    'KeyType': 'HASH'  # Partition key
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'URL_ADDRESS',
+                    'AttributeType': 'S'
+                }
+    
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+        )
+        time.sleep(5)
     except:
         pass
 
 
 def putting_sprint3_data():
-    try:
-        URLs = s3bucket.read_file("abdullahzamanbucket", "urlsList.json")
-        client_ = boto3.client('dynamodb')
-        for U in URLs:
-            item = {
-                'URL_ADDRESS': {'S': U}
-                    }
-            client_.put_item(TableName="AbdullahSprint3", Item=item)
-    except:
-        pass
+    URLs = s3bucket.read_file("abdullahzamanbucket", "urlsList.json")
+    client_ = boto3.client('dynamodb')
+    for U in URLs:
+        item = {
+            'URL_ADDRESS': {'S': U}
+                }
+        print(item)
+        client_.put_item(TableName="AbdullahSprint3", Item=item)
