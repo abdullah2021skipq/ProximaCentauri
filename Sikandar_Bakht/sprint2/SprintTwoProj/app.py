544ef61cdf65adb19d5f681a5a3a9cba8ec9cf06
Ubuntu <ubuntu@ip-172-31-35-179.us-east-2.compute.internal>
2021-12-29 15:55:25 +0000

    redeploy

--- Sikandar_Bakht/sprint2/SprintTwoProj/app.py
+++ Sikandar_Bakht/sprint2/SprintTwoProj/app.py
@@ -7,7 +7,7 @@ from sprint_two_proj.sikandar_pipeline_stack import SikandarPipelineStack
 
 
 app = core.App()
-print("triggers")
+print("triggers2")
 SikandarPipelineStack(app, "sikandarpipeline", env=core.Environment(account = '315997497220', region = 'us-east-2'))
 
 app.synth()
