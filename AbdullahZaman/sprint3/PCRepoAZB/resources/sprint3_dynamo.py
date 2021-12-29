cba916c311a26cb519bfe2970400e425fb2988e1
Ubuntu <ubuntu@ip-172-31-39-2.us-east-2.compute.internal>
2021-12-29 20:54:07 +0000

    sprint3 dynamo

--- AbdullahZaman/sprint3/PCRepoAZB/resources/sprint3_dynamo.py
+++ AbdullahZaman/sprint3/PCRepoAZB/resources/sprint3_dynamo.py
@@ -1,13 +1,35 @@
-import boto3,os
+import boto3
 from resources import s3bucket
-#import json
 
-def lambda_handler(event, context):
-    #write to dynamodb database
-    URLs = s3bucket.read_file("abdullahzamanbucket", "urlsList.json")
-    client_ = boto3.client('dynamodb')
-    for U in URLs:
-        item = {
-            'URL_ADDRESS': {'S': U}
-                }
-        client_.put_item(TableName="AbdullahSprint3", Item=item)
+def create_sprint3_table():
+    client_ = boto3.resource('dynamodb', region_name='us-east-2')
+    try:
+        table = client_.create_table(TableName='AbdullahSprint3',
+                                KeySchema=[
+                                {
+                                'AttributeName': 'URL_ADDRESS',
+                                'KeyType': 'HASH'
+                                }
+                    ],
+                                AttributeDefinitions=[
+                                {
+                                'AttributeName': 'URL_ADDRESS',
+                                'AttributeType' : 'S'
+                                }
+                                ]
+                    )
+    except:
+        pass
+
+
+def putting_sprint3_data():
+    try:
+        URLs = s3bucket.read_file("abdullahzamanbucket", "urlsList.json")
+        client_ = boto3.client('dynamodb')
+        for U in URLs:
+            item = {
+                'URL_ADDRESS': {'S': U}
+                    }
+            client_.put_item(TableName="AbdullahSprint3", Item=item)
+    except:
+        pass
