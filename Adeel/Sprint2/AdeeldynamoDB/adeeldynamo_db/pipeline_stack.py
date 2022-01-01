085dd8660bf3c49431b6c5d0ea47e597443f6bc9
EC2 Default User <ec2-user@ip-172-31-38-191.us-east-2.compute.internal>
2022-01-01 08:00:46 +0000

    Updating readMe

--- Adeel/Sprint2/AdeeldynamoDB/adeeldynamo_db/pipeline_stack.py
+++ Adeel/Sprint2/AdeeldynamoDB/adeeldynamo_db/pipeline_stack.py
@@ -42,14 +42,13 @@ class PipelineStack(core.Stack):
         })
         
          ############################## unit test ###############################
-        
+
         unit_test = pipelines.ShellStep('unit_test',
         commands=["cd Adeel/Sprint2/AdeeldynamoDB","pip install -r requirements.txt" ,
         "pytest unittests","pytest integtests"])
-        
          ############################## adding stages ###############################
         
-        pipeline.add_stage(beta, pre = [unit_test])
+        pipeline.add_stage(beta , pre = [unit_test])
     
         pipeline.add_stage(prod ,
         pre = [pipelines.ManualApprovalStep("PromoteToProd")])
