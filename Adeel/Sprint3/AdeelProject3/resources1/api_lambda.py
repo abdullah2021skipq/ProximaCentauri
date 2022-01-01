cfcd8b888675809475a6003c8ede22ed23ec1769
EC2 Default User <ec2-user@ip-172-31-38-191.us-east-2.compute.internal>
2022-01-01 18:23:30 +0000

    final test for api

--- Adeel/Sprint3/AdeelProject3/resources1/api_lambda.py
+++ Adeel/Sprint3/AdeelProject3/resources1/api_lambda.py
@@ -9,35 +9,36 @@ def lambda_handler(events, context):
     if events['httpMethod'] == 'GET':
         
         data = dynamo_RW.ReadFromTable(os.getenv(key = 'table_name'))
-        response_msg = data
+        response_msg = f"data from table is = {data} "
     
     elif events['httpMethod'] == 'PUT':
         
         
-        new_url = event['body']
+        new_url = events['body']
         client.put_item(
         TableName = os.getenv(key='table_name'),
         Item={
         'Links':{'S' : new_url},
         })
-        response_msg = 'Url is successfully added into the table'
+        response_msg = f"Url = {events['body']} is successfully added into the table"
         
     elif events['httpMethod'] == 'DELETE':
     
-        new_url = event['body']
+        new_url = events['body']
+        print(new_url)
         client.delete_item(
         TableName = os.getenv(key='table_name'),
-        Item={
-        'Links':{'S' : new_url},
+        Key={
+        'Links':{'S' : new_url}
         })
-        response_msg = 'Url is successfully deleted from the table'
+        response_msg = f"Url= {events['body']} is successfully deleted from the table"
     else:
         
         response_msg = 'You can only put a item in table, delete it or get items from table'
+    print(response_msg)  
+    return {
         
-    return{
-        
-        'statusCode' : 200
+        'statusCode' : 200,
         'body'  :  response_msg
         
     }
