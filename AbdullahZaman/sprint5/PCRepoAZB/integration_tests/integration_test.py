472433027d357c64c184c2e516518bcacb0d4cfd
Ubuntu <ubuntu@ip-172-31-39-2.us-east-2.compute.internal>
2022-01-20 02:54:43 +0000

    a

--- AbdullahZaman/sprint5/PCRepoAZB/integration_tests/integration_test.py
+++ AbdullahZaman/sprint5/PCRepoAZB/integration_tests/integration_test.py
@@ -7,7 +7,7 @@ import json
 apis = json.load(os.popen('aws apigateway get-rest-apis'))  # popen runs the cli commands
 apis = apis["items"]
 for dic in apis:
-    if dic["name"] == "AzbApi":
+    if dic["name"] == "AzbApisprint5":
         api_ID = dic["id"]
         break
 
