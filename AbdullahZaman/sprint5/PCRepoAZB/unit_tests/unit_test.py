9a40880f1a669a8460b8a179448550a94dd54633
Ubuntu <ubuntu@ip-172-31-39-2.us-east-2.compute.internal>
2022-01-20 00:55:42 +0000

    a

--- AbdullahZaman/sprint5/PCRepoAZB/unit_tests/unit_test.py
+++ AbdullahZaman/sprint5/PCRepoAZB/unit_tests/unit_test.py
@@ -19,4 +19,4 @@ def test_alarm_count():
     template=app.synth().get_stack_by_name('Unittest').template
     fun=[resource for resource in template['Resources'].values() if resource['Type']=='AWS::CloudWatch::Alarm']
 
-    assert len(fun)>=1
+    assert len(fun)>1
